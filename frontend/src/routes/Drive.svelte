57e2bdb672d867aa99d4ab021797664605ba8a53
AbP-sys <abhishekpagadala@gmail.com>
2024-02-23 17:54:48 +0530

    clickable breadcrumb

--- frontend/src/routes/Drive.svelte
+++ frontend/src/routes/Drive.svelte
@@ -1,5 +1,5 @@
 <script>
-  import { Breadcrumb, BreadcrumbItem, Card } from "flowbite-svelte";
+  import { Breadcrumb, BreadcrumbItem, Button, Card } from "flowbite-svelte";
   import { onMount } from "svelte";
   import { FileOutline, FolderOutline } from "flowbite-svelte-icons";
 
@@ -8,6 +8,7 @@
   let path = "";
   let segments = [];
   $: segments = path.split("/").filter(Boolean);
+
   async function fetchData(folder) {
     path += folder;
     console.log(path);
@@ -20,12 +21,38 @@
   onMount(() => {
     fetchData(path);
   });
+
+  function handleBackClick(index) {
+    if (index < 0) {
+      path = "";
+    } else {
+      const newPath = segments.slice(0, index + 1).join("/");
+      path = "/" + newPath;
+    }
+    fetchData("");
+  }
 </script>
 
 <Breadcrumb aria-label="Default breadcrumb">
-  <BreadcrumbItem href="/" home>root</BreadcrumbItem>
-  {#each segments as segment (segment)}
-    <BreadcrumbItem>{segment}</BreadcrumbItem>
+  <BreadcrumbItem home
+    ><Button
+      color="alternative"
+      class="bg-transparent !p-1 border-transparent"
+      on:click={() => handleBackClick(-1)}
+    >
+      root
+    </Button></BreadcrumbItem
+  >
+  {#each segments as segment, index (segment)}
+    <BreadcrumbItem
+      ><Button
+        color="alternative"
+        class="bg-transparent !p-1 border-transparent"
+        on:click={() => handleBackClick(index)}
+      >
+        {segment}
+      </Button>
+    </BreadcrumbItem>
   {/each}
 </Breadcrumb>
 
